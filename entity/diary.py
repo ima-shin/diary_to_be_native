dc1806444bb4c30b4c38258806fd421a7ffb5187
Shinpe1 <Shinpei8080@gmail.com>
2021-02-17 21:21:51 +0900

    DBへの向き先をローカルのSQLite3に変更

--- entity/diary.py
+++ entity/diary.py
@@ -6,16 +6,11 @@ from settings import *
 class Diary(object):
     """日記エンティティ"""
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self):
         super().__init__()
 
         self.id = str(uuid.uuid4())
         self.content = ''
+        self.written_date = str(datetime.datetime.now(JST).strftime(DATE_FORMAT))
         self.created_at = str(datetime.datetime.now(JST).strftime(DATE_FORMAT))
         self.updated_at = str(datetime.datetime.now(JST).strftime(DATE_FORMAT))
-
-    def created_at_str(self):
-        return str(self.created_at)
-
-    def updated_at_str(self):
-        return str(self.updated_at)
